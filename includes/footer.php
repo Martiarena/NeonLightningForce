73e6aaccabff6442941cffdbbc165f81102b1b7e
Raúl Martiarena <raulmartiarena89@gmail.com>
2022-10-27 10:53:36 -0500

    Creditos

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,110 @@
+<div id="inline2" style="display: none; text-align:center;">
+	<div style="width:100%; background: black;">
+        <div class="text-credits">
+            <?php if ($cod_idioma==1){?>
+            <h4 class="version">Credits and Resources</h4>
+            <p>Graphics, music and sound effects belong to the following listed games:</p>
+            <h5>Graphics</h5>
+            <p>From spriters-resource.com & spritedatabase.net:</p>
+            <ul>
+                <li>Final Fight 1 & 2</li>
+                <li>Street of Rage 2</li>
+                <li>Pretty Soldier Sailor Moon</li>
+                <li>Karnov's Revenge</li>
+                <li>Double Dragon</li>
+                <li>Savage Reign</li>
+                <li>Fight Fever</li>
+                <li>Guardians</li>
+                <li>Street Fighter 2</li>
+                <li>Ghost Chaser Densei</li>
+                <li>Cadillacs & Dinosaurs</li>
+            </ul>
+            <h5>Music</h5>
+            <ul>
+                <li>Ryo ga Gotoku</li>
+                <li>Ryo ga Gotoku 2</li>
+                <li>Ryo ga Gotoku 3</li>
+                <li>Ryo ga Gotoku 4</li>
+                <li>Ryo ga Gotoku 5</li>
+                <li>King of Fighters</li>
+                <li>Crisis Beat</li>
+                <li>Bakuretsu Tenshi</li>
+                <li>The Bouncer</li>
+                <li>Crimson Tears</li>
+            </ul>
+            <h5>Ending Theme</h5>
+            <ul>
+                <li>[BLOODY MOON] (by Gospel of Judas)</li>
+            </ul>
+            <h5>Sound Effect</h5>
+            <ul>
+                <li>Final Fight</li>
+                <li>Streets of Rage 2</li>
+                <li>Guardians</li>
+                <li>Denjin Makai</li>
+                <li>Ghost Chaser Densei</li>
+                <li>Crimson Tears</li>
+                <li>Cadillacs & Dinosaurs</li>
+            </ul>
+            <h5>Engine Used</h5>
+            <ul>
+                <li>Beats of Rage (OpenBOR)</li>
+            </ul>
+            <?php }else{ ?>
+            <h4 class="version">Cr&eacute;ditos y Recursos</h4>
+            <p>Gr&aacute;ficos, m&uacute;sica y efectos de sonido pertenecen a los siguientes juegos listados:</p>
+            <h5>Gr&aacute;ficos</h5>
+            <p>De spriters-resource.com & spritedatabase.net:</p>
+            <ul>
+                <li>Final Fight 1 & 2</li>
+                <li>Street of Rage 2</li>
+                <li>Pretty Soldier Sailor Moon</li>
+                <li>Karnov's Revenge</li>
+                <li>Double Dragon</li>
+                <li>Savage Reign</li>
+                <li>Fight Fever</li>
+                <li>Guardians</li>
+                <li>Street Fighter 2</li>
+                <li>Ghost Chaser Densei</li>
+                <li>Cadillacs & Dinosaurs</li>
+            </ul>
+            <h5>M&uacute;sica</h5>
+            <ul>
+                <li>Ryo ga Gotoku</li>
+                <li>Ryo ga Gotoku 2</li>
+                <li>Ryo ga Gotoku 3</li>
+                <li>Ryo ga Gotoku 4</li>
+                <li>Ryo ga Gotoku 5</li>
+                <li>King of Fighters</li>
+                <li>Crisis Beat</li>
+                <li>Bakuretsu Tenshi</li>
+                <li>The Bouncer</li>
+                <li>Crimson Tears</li>
+            </ul>
+            <h5>Tema de Cierre:</h5>
+            <ul>
+                <li>[BLOODY MOON] (por Gospel of Judas)</li>
+            </ul>
+            <h5>Efectos de sonido</h5>
+            <ul>
+                <li>Final Fight</li>
+                <li>Streets of Rage 2</li>
+                <li>Guardians</li>
+                <li>Denjin Makai</li>
+                <li>Ghost Chaser Densei</li>
+                <li>Crimson Tears</li>
+                <li>Cadillacs & Dinosaurs</li>
+            </ul>
+            <h5>Motor Usado</h5>
+            <ul>
+                <li>Beats of Rage (OpenBOR)</li>
+            </ul>
+            <?php } ?>
+        </div>
+	</div>
+</div>
 <footer>
-	<p>&copy; <a href="http://www.trueroledreams.org/">True Role Dreams</a> 2016. <?php if ($cod_idioma==1){?>Some rights reserved x.x.<?php }else{ ?>Algunos derechos reservados x.x.<?php } ?> | <?php if ($cod_idioma==1){?><a class="fancybox" href="img/credits-beat-em-up-openbor-indie-game.jpg">Credits & resources</a><?php }else{ ?><a class="fancybox" href="img/creditos-e-beat-em-up-openbor-indie-game.jpg">Cr&eacute;ditos y recursos</a><?php } ?></p>
+	<p>&copy; <a href="http://www.trueroledreams.org/">True Role Dreams</a> 2016. <?php if ($cod_idioma==1){?>Some rights reserved x.x.<?php }else{ ?>Algunos derechos reservados x.x.<?php } ?> | <?php if ($cod_idioma==1){?><a class="fancybox" href="#inline2">Credits & resources</a><?php }else{ ?><a class="fancybox" href="#inline2">Cr&eacute;ditos y recursos</a><?php } ?></p>
 </footer>
 <script type="application/javascript" src="https://a.exdynsrv.com/video-slider.js"></script>
 <script type="application/javascript">
